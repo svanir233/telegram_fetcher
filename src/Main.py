f8e8b5e7dacb6321a02d29cae3984d8a8df5e2fe
Colin Chen <colin.chen@live.com>
2023-09-18 21:06:24 +0800

    udpate

--- src/Main.py
+++ src/Main.py
@@ -1,55 +1,56 @@
-import pandas as pd
-from telegram import Bot
+from telethon.sync import TelegramClient
+import csv
+import json
+import datetime
 
-# Your Telegram Bot Token
-BOT_TOKEN = "6591582102:AAF_v5S5X1ircq1u3YetDFlj5i7YerB58ss"
+# Your API ID and API hash (obtain these from my.telegram.org)
+api_id = 29592935
+api_hash = '7e41b018b51c7a4f62d54af120a58ce2'
 
-# The group chat ID you want to query
-GROUP_CHAT_ID = -1001975455133  # Use a negative value to represent a group chat ID
+# Your bot token
+bot_token = '6591582102:AAF_v5S5X1ircq1u3YetDFlj5i7YerB58ss'
 
-def get_group_members_info(bot_token, chat_id):
-    bot = Bot(token=bot_token)
-    members_info = []
+# Initialize the client
+client = TelegramClient('mnlwin_bot_session', api_id, api_hash)
 
-    # Get the count of members in the group
-    members_count = bot.get_chat_members_count(chat_id)
-
-    # Fetch information about each member
-    members = bot.get_chat_members(chat_id)
-    
-    for member in members:
-        user_id = member.user.id
-        username = member.user.username
-        first_name = member.user.first_name
-        last_name = member.user.last_name
-        language_code = member.user.language_code
-        status = member.status
-        join_date = member.date  # Get the join date
+async def main():
+    await client.start(bot_token=bot_token)
+        # Search for the group chat by title (replace 'GroupName' with your group name)
+    group_entity = await client.get_entity(-1001975455133)
         
-        # Append member information to the list
-        members_info.append({
-            "User ID": user_id,
-            "Username": username,
-            "First Name": first_name,
-            "Last Name": last_name,
-            "Language Code": language_code,
-            "Status": status,
-            "Join Date": join_date,
-        })
+    # Get the group chat members
+    members = []
+    async for member in client.iter_participants(group_entity):
+    # Convert member object to dictionary
+        member_dict = member.to_dict()
+        
+        # Append member information as a dictionary
+        members.append(member_dict)
 
-    return members_info
+    # Log out at the end of your operations
+    await client.log_out()
 
-def save_member_info_to_csv(members_info, filename):
-    # Create a DataFrame
-    df = pd.DataFrame(members_info)
-    
-    # Save it as a CSV file
-    df.to_csv(filename, index=False)
+    # Generate a timestamp for the current time
+    current_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 
-if __name__ == "__main__":
-    members_info = get_group_members_info(BOT_TOKEN, GROUP_CHAT_ID)
-    if members_info:
-        save_member_info_to_csv(members_info, "group_members_info.csv")
-        print("Member information saved to group_members_info.csv")
-    else:
-        print("No members found in the group or information is not available.")
+    # Save members information to a CSV file
+    with open(f'group_members_{current_time}.csv', 'w', newline='', encoding='utf-8') as csv_file:
+        # Extract column names from the first member's keys
+        column_names = members[0].keys()
+        
+        writer = csv.DictWriter(csv_file, fieldnames=column_names)
+        
+        # Write header row
+        writer.writeheader()
+        
+        # Write member information rows
+        writer.writerows(members)
+
+    # Save members information to a JSON file
+    with open(f'group_members{current_time}.json', 'w', encoding='utf-8') as json_file:
+        json.dump(members, json_file, ensure_ascii=False, indent=4)
+
+if __name__ == '__main__':
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
